4e68d3143ea960f98bf76c8298400208accaf1b5
Renata Cioltea <renatacioltea@gmail.com>
2024-11-13 13:11:03 +0200

    add firebase config

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,11 @@
 import {
-  sinInWithGooglePopup,
+  signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const { user } = await sinInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
   return (
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: "capstone-db-9b548.firebaseapp.com",
+  projectId: "capstone-db-9b548",
+  storageBucket: "capstone-db-9b548.firebasestorage.app",
+  messagingSenderId: "583581501722",
+  appId: "1:583581501722:web:c80847d6cd1d20557544a3",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
