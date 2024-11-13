2583cc601a524f3a21d19365cb1af03dbb99cf79
Renata Cioltea <renatacioltea@gmail.com>
2024-11-13 12:01:47 +0200

    create signin page

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,19 @@
+import {
+  sinInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await sinInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div>
+      <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
